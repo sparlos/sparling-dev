314b427ba89cdb40adb87c9073047f91e295ad4a
Stephen Sparling <sparlingcreations@gmail.com>
2022-09-01 09:19:45 -0400

    add glow for dark mode on home screen, clean up motion

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
-import { motion } from 'framer-motion'
+import { motion, TargetAndTransition } from 'framer-motion'
 import { IconType } from 'react-icons'
 import { useState } from 'react'
+import { DEFAULT_SPRING_TRANSITION } from '../utils/framer'
 
 type LinkButtonProps = {
   className?: string
@@ -9,6 +10,11 @@ type LinkButtonProps = {
   label: string
   href: string
   Icon?: IconType
+}
+
+const BUTTON_ACTIVE_ANIMATION: TargetAndTransition = {
+  x: 12,
+  y: -12,
 }
 
 export default function LinkButton({
@@ -22,28 +28,31 @@ export default function LinkButton({
 
   return (
     <Link href={href}>
-      <motion.button
+      <button
         onFocus={() => setIsButtonFocused(true)}
         onBlur={() => setIsButtonFocused(false)}
         className={'relative w-32 h-28 ' + className}
       >
         <motion.div
-          className="flex relative z-10 flex-col p-4 w-full h-full items-center shadow-md rounded bg-white dark:bg-slate-700"
-          animate={isButtonFocused ? { x: 10, y: -10 } : {}}
-          whileHover={{
-            x: 10,
-            y: -10,
-          }}
+          className="peer flex relative z-10 flex-col p-4 w-full h-full items-center shadow-md rounded bg-white dark:bg-slate-700"
+          transition={DEFAULT_SPRING_TRANSITION}
+          animate={isButtonFocused ? BUTTON_ACTIVE_ANIMATION : {}}
+          onHoverStart={() => setIsButtonFocused(true)}
+          onHoverEnd={() => setIsButtonFocused(false)}
         >
           {Icon && <Icon className="mb-2 mt-4" />}
           <div>{label}</div>
         </motion.div>
-        <motion.div
+        <div
+          className={'absolute inset-0 rounded-md w-full h-full z-0 ' + bgColor}
+        />
+        <div
           className={
-            'absolute top-0 left-0 rounded w-full h-full z-0 ' + bgColor
+            'opacity-0 dark:opacity-25 absolute blur scale-125 peer-hover:scale-50 transition duration-500 p-6 inset-0 rounded-md w-full h-full z-0 ' +
+            bgColor
           }
         />
-      </motion.button>
+      </button>
     </Link>
   )
 }
---
+++ utils/framer.ts
@@ -0,0 +1,7 @@
+import { Transition } from "framer-motion";
+
+export const DEFAULT_SPRING_TRANSITION: Transition = {
+  type: 'spring',
+  stiffness: 150,
+  damping: 20
+}
