3b640d60cfd368d35baec8425350c757da3131db
Stephen Sparling <sparlingcreations@gmail.com>
2022-09-01 13:10:52 -0400

    install usehooks-ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Red Hat Display"', 'ui-sans-serif', 'system-ui']
+        sans: ['"Red Hat Display"', 'ui-sans-serif', 'system-ui'],
       },
     },
   },
