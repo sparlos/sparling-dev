7f9a9753d72358c4fd18c378815a596fba1b1998
Stephen Sparling <sparlingcreations@gmail.com>
2022-08-31 22:41:05 -0400

    flesh out index screen

---
+++ components/LinkButton.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { IconType } from 'react-icons'
+import { useState } from 'react'
+
+type LinkButtonProps = {
+  className?: string
+  bgColor?: string
+  label: string
+  href: string
+  Icon?: IconType
+}
+
+export default function LinkButton({
+  className,
+  bgColor = 'bg-sky-400',
+  label,
+  href,
+  Icon,
+}: LinkButtonProps) {
+  const [isButtonFocused, setIsButtonFocused] = useState(false)
+
+  return (
+    <Link href={href}>
+      <motion.button
+        onFocus={() => setIsButtonFocused(true)}
+        onBlur={() => setIsButtonFocused(false)}
+        className={'relative w-32 h-28 ' + className}
+      >
+        <motion.div
+          className="flex relative z-10 flex-col p-4 w-full h-full items-center shadow-md rounded bg-white"
+          animate={isButtonFocused ? { x: 10, y: -10 } : {}}
+          whileHover={{
+            x: 10,
+            y: -10,
+          }}
+        >
+          {Icon && <Icon className="mb-2 mt-4" />}
+          <div>{label}</div>
+        </motion.div>
+        <motion.div
+          className={
+            'absolute top-0 left-0 rounded w-full h-full z-0 ' + bgColor
+          }
+        />
+      </motion.button>
+    </Link>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 import type { NextPage } from 'next'
+import LinkButton from '../components/LinkButton'
+import { FiUser, FiCode, FiSend } from 'react-icons/fi'
 
 const Home: NextPage = () => {
   return (
-    <div className="items-center justify-center flex h-full flex-col text-center">
-      <h1 className="text-6xl font-sans font-semibold">stephen sparling</h1>
-      <h2 className="mt-8">web developer | making cool stuff</h2>
+    <div className="dark:bg-slate-700 items-center flex h-full flex-col text-center">
+      <div className="my-auto flex items-center flex-col">
+        <h1 className="text-6xl font-sans font-semibold dark:text-white">stephen sparling</h1>
+        <h2 className="mt-8 dark:text-white">web developer | making cool stuff</h2>
+        <div className="mt-12 flex flex-col sm:flex-row">
+          <LinkButton
+            Icon={FiUser}
+            href="/about"
+            label="who I am"
+            className="sm:mr-24 mb-6 sm:mb-0"
+          />
+          <LinkButton
+            Icon={FiCode}
+            href="/about"
+            label="what I do"
+            className="sm:mr-24 mb-6 sm:mb-0"
+            bgColor="bg-red-500"
+          />
+          <LinkButton
+            Icon={FiSend}
+            href="/about"
+            label="where I'm found"
+            bgColor="bg-violet-500"
+          />
+        </div>
+      </div>
     </div>
   )
 }
