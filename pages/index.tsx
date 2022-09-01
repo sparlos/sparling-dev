6953c60272644d934b7b930994cc9407dfb176a4
Stephen Sparling <sparlingcreations@gmail.com>
2022-08-31 21:49:36 -0400

    configure font, prettier, start beginnings of first page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import { motion } from 'framer-motion'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <motion.p className="text-3xl font-bold underline" animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-        hello world
-      </motion.p>
+    <div className="items-center justify-center flex h-full flex-col text-center">
+      <h1 className="text-6xl font-sans font-semibold">stephen sparling</h1>
+      <h2 className="mt-8">web developer | making cool stuff</h2>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Red Hat Display"', 'ui-sans-serif', 'system-ui']
+      },
+    },
   },
   plugins: [],
 }
