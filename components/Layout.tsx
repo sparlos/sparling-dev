072098533ffb377576adc6fa2c157fc80c98f639
Stephen Sparling <sparlingcreations@gmail.com>
2022-09-02 16:10:10 -0400

    set layout header to highest z index

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex h-full w-full transition-colors duration-200 dark:bg-gradient-to-b dark:from-slate-600 dark:to-slate-700">
       <div className="container relative mx-auto flex h-full w-full flex-col">
-        <div className="pointer-events-none absolute inset-0 flex h-full w-full items-start justify-end py-8 px-8">
+        <div className="pointer-events-none absolute inset-0 z-50 flex h-full w-full items-start justify-end py-8 px-8">
           {mounted && (
             <div className="relative rounded shadow-md">
               <motion.button
