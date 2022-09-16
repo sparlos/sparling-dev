1b8b16c26c73475c6d3b5f5c368bf3e6aa1755b8
Stephen Sparling <sparlingcreations@gmail.com>
2022-09-16 18:05:50 -0400

    rough out navbar button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'next-themes'
-import { ReactNode, useEffect, useState } from 'react'
-import { FiMoon, FiSun } from 'react-icons/fi'
+import { Fragment, ReactNode, useEffect, useState } from 'react'
+import { FiMenu, FiMoon, FiSun } from 'react-icons/fi'
 import { motion } from 'framer-motion'
 import { DEFAULT_SPRING_TRANSITION } from '../utils/framer'
 
@@ -11,6 +11,8 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   const [mounted, setMounted] = useState(false)
   const [isDarkModeToggleHovered, setIsDarkModeToggleHovered] = useState(false)
+  const [isMenuButtonHovered, setIsMenuButtonHovered] = useState(false)
+  const [isMenuActive, setIsMenuActive] = useState(false)
   const { theme, systemTheme, setTheme } = useTheme()
 
   const currentTheme = theme !== 'system' ? theme : systemTheme
@@ -24,36 +26,66 @@ export default function Layout({ children }: LayoutProps) {
       <div className="container relative mx-auto flex h-full w-full flex-col">
         <div className="pointer-events-none absolute inset-0 z-50 flex h-full w-full items-start justify-end py-8 px-8">
           {mounted && (
-            <div className="relative rounded shadow-md">
-              <motion.button
-                aria-label={`turn dark mode ${
-                  currentTheme === 'dark' ? 'off' : 'on'
-                }`}
-                onHoverStart={() => setIsDarkModeToggleHovered(true)}
-                onHoverEnd={() => setIsDarkModeToggleHovered(false)}
-                className="peer pointer-events-auto relative z-10 block h-full w-full rounded bg-white p-4 dark:bg-slate-700"
-                onClick={() =>
-                  setTheme(currentTheme === 'dark' ? 'light' : 'dark')
-                }
-              >
-                <motion.div
-                  animate={{
-                    rotate: isDarkModeToggleHovered ? 360 : 0,
-                  }}
-                  transition={DEFAULT_SPRING_TRANSITION}
+            <Fragment>
+              <div className="relative rounded shadow-md">
+                <motion.button
+                  aria-label={`turn dark mode ${
+                    currentTheme === 'dark' ? 'off' : 'on'
+                  }`}
+                  onHoverStart={() => setIsDarkModeToggleHovered(true)}
+                  onHoverEnd={() => setIsDarkModeToggleHovered(false)}
+                  className="peer pointer-events-auto relative z-10 block h-12 w-12 rounded bg-white p-4 dark:bg-slate-700"
+                  onClick={() =>
+                    setTheme(currentTheme === 'dark' ? 'light' : 'dark')
+                  }
                 >
-                  <FiSun
-                    className={currentTheme === 'dark' ? 'block' : 'hidden'}
-                  />
-                  <FiMoon
-                    className={currentTheme === 'light' ? 'block' : 'hidden'}
-                  />
-                </motion.div>
-              </motion.button>
-              {currentTheme === 'dark' && (
-                <div className="absolute inset-0 h-full w-full scale-105 rounded-md bg-white opacity-10 blur-sm transition duration-300 peer-hover:scale-125" />
-              )}
-            </div>
+                  <motion.div
+                    animate={{
+                      rotate: isDarkModeToggleHovered ? 360 : 0,
+                    }}
+                    transition={DEFAULT_SPRING_TRANSITION}
+                  >
+                    <FiSun
+                      className={currentTheme === 'dark' ? 'block' : 'hidden'}
+                    />
+                    <FiMoon
+                      className={currentTheme === 'light' ? 'block' : 'hidden'}
+                    />
+                  </motion.div>
+                </motion.button>
+                {currentTheme === 'dark' && (
+                  <div className="absolute inset-0 h-full w-full scale-105 rounded-md bg-white opacity-10 blur-sm transition duration-300 peer-hover:scale-125" />
+                )}
+              </div>
+              <div className="relative ml-4 rounded shadow-md">
+                <motion.button
+                  aria-label={`turn dark mode ${
+                    currentTheme === 'dark' ? 'off' : 'on'
+                  }`}
+                  onFocus={() => setIsMenuButtonHovered(true)}
+                  onBlur={() => setIsMenuButtonHovered(false)}
+                  onHoverStart={() => setIsMenuButtonHovered(true)}
+                  onHoverEnd={() => setIsMenuButtonHovered(false)}
+                  className="pointer-events-auto relative z-10 flex items-center overflow-hidden rounded bg-white p-4 dark:bg-slate-700"
+                  onClick={() => setIsMenuActive(!isMenuActive)}
+                  initial={{ width: '3rem', height: '3rem' }}
+                  animate={{
+                    width: isMenuActive ? '12rem' : '3rem',
+                    height: isMenuActive ? '16rem' : '3rem',
+                  }}
+                >
+                  <motion.div
+                    initial={{ rotate: 0, y: 0 }}
+                    animate={{
+                      rotate: isMenuButtonHovered ? 90 : 0,
+                      y: isMenuActive ? -150 : 0,
+                    }}
+                  >
+                    <FiMenu />
+                  </motion.div>
+                </motion.button>
+              </div>
+            </Fragment>
           )}
         </div>
         <main className="h-full w-full">{children}</main>
