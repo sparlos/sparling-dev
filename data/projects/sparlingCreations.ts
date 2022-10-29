7360d69834630a8890d483fc99c03dc4d860c81c
Stephen Sparling <sparlingcreations@gmail.com>
2022-10-29 11:17:40 -0400

    update sparling creations project with content

--- data/projects/sparlingCreations.ts
+++ data/projects/sparlingCreations.ts
@@ -4,16 +4,29 @@ import coverImage from '../../public/projects/sparlingCreations/cover.png'
 const sparlingCreations: Project = {
   title: 'Sparling Creations',
   slug: 'sparling-creations',
-  tags: ['vueJS', 'Audio'],
+  tags: ['vueJS', 'Audio', 'SCSS'],
   images: {
     cover: coverImage,
     portrait: [],
-    landscape: [],
+    landscape: [coverImage],
   },
-  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis natus
-  nam ducimus aspernatur corrupti inventore dolorem libero ut voluptates
-  dolorum. Ad, rerum quod. Ipsa, aspernatur sint. Harum doloremque dolorem
-  quia.`,
+  description: `
+  <p class="project-description__paragraph">
+    A website built to house my freelance web design, video and audio
+    projects. The site is powered by Vue and uses Vue Router for
+    navigation.
+  </p>
+  <p class="project-description__paragraph">
+    The design was created completely from scratch in Adobe XD, with
+    animations and transitions all hand-coded using SCSS and BEM
+    methodology. I wrote a mini-app in Vue that wraps the SoundCloud
+    iFrame API in the 'Music' section of the site. This was done for
+    aesthetic reasons in order to make the design of the player
+    compliment the design of the site. The data layer of the site is
+    powered using Cockpit CMS allowing for easy addition of new
+    projects.
+  </p>
+  `,
 }
 
 export default sparlingCreations
